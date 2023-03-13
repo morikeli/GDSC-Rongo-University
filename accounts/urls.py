b761749903e1cd1a2bd70437d17e32033d3c4e18
brocode <kelimbth@gmail.com>
2023-03-13 11:50:50 +0300

    added accounts URL paths

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.UsersLoginView.as_view(), name='user_login'),
+    path('profile/', views.userprofile_view, name='user_profile'),
+
+    path('logout/', views.LogoutUser.as_view(), name='logout_user'),
+]
