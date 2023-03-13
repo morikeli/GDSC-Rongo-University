868c886514218f983a9becce146c43c5a3f2fa86
brocode <kelimbth@gmail.com>
2023-03-13 11:50:22 +0300

    created login, profile and logout views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import LoginForm, SignupForm, UpdateProfileForm
 
-# Create your views here.
+
+class UsersLoginView(LoginView):
+    authentication_form = LoginForm
+    template_name = 'accounts/login.html'
+
+def signup_view(request):
+    form = SignupForm()
+    
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Acoount created succesfully!')
+            return redirect('user_login')
+
+    context = {'form': form}
+    return render(request, 'accounts/signup.html', context)
+
+@login_required(login_url='user_login')
+@user_passes_test(lambda user: user.is_superuser is False)
+def userprofile_view(request):
+    form = UpdateProfileForm(instance=request.user)
+
+    if request.method == 'POST':
+        form = UpdateProfileForm(request.POST, request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.info(request, 'Profile updated successfully!')
+            return redirect('user_profile')
+
+
+    context = {'form': form}
+    return render(request, 'accounts/profile.html', context)
+
+
+class LogoutUser(LogoutView):
+    template_name = 'accounts/logout.html'
