a2d754306f1f54d98a0b2bd960384e16e6057a8f
brocode <kelimbth@gmail.com>
2023-03-14 18:23:52 +0300

    minor change

--- static/js/main.js
+++ static/js/main.js
@@ -101,8 +101,8 @@
 	// mobile nav dropdowns activate
 	on('click', '.navbar .dropdown > a', function(e) {
 		if (select('#navbar').classList.contains('navbar-mobile')) {
-		e.preventDefault()
-		this.nextElementSibling.classList.toggle('dropdown-active')
+            e.preventDefault()
+            this.nextElementSibling.classList.toggle('dropdown-active')
 		}
 	}, true)
 
