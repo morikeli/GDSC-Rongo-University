9c73b5a95ec90891d13d2450973b6938252a64a2
brocode <kelimbth@gmail.com>
2023-03-14 18:02:32 +0300

    minor changes

--- static/js/main.js
+++ static/js/main.js
@@ -141,37 +141,37 @@
 	});
 
 	// gallery slider
-	new Swiper('.gallery-slider', {
+	new Swiper('.testimonials', {
 		speed: 400,
 		loop: true,
 		centeredSlides: true,
 		autoplay: {
-		delay: 5000,
-		disableOnInteraction: false
+            delay: 5000,
+            disableOnInteraction: false
 		},
 		slidesPerView: 'auto',
 		pagination: {
-		el: '.swiper-pagination',
-		type: 'bullets',
-		clickable: true
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
 		},
 		breakpoints: {
-		320: {
-			slidesPerView: 1,
-			spaceBetween: 20
-		},
-		575: {
-			slidesPerView: 2,
-			spaceBetween: 20
-		},
-		768: {
-			slidesPerView: 3,
-			spaceBetween: 20
-		},
-		992: {
-			slidesPerView: 5,
-			spaceBetween: 20
-		}
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+            575: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            },
+            992: {
+                slidesPerView: 5,
+                spaceBetween: 20
+            }
 		}
 	});
 
