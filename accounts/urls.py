feddcd5f89019cfec31e5647392278e5555d7483
brocode <kelimbth@gmail.com>
2023-04-02 21:12:58 +0300

    created accounts template

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.UsersLoginView.as_view(), name='user_login'),
+    path('signup/', views.signup_view, name='signup'),
     path('profile/', views.userprofile_view, name='user_profile'),
 
     path('logout/', views.LogoutUser.as_view(), name='logout_user'),
